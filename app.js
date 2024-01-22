e9bf204217c4ac03378f5edf771382054fc9f763
Ganesh Rajkumar Shete <136620219+Code-Crafter-Ganesh@users.noreply.github.com>
2024-01-22 13:42:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
+// for(code in countryList){
+//     console.log(code,countryList[code]);
+// }
+
 for (let select of dropdowns) {
   for (currCode in countryList) {
     let newOption = document.createElement("option");
@@ -27,22 +31,35 @@ for (let select of dropdowns) {
 
 const updateExchangeRate = async () => {
   let amount = document.querySelector(".amount input");
+
   let amtVal = amount.value;
   if (amtVal === "" || amtVal < 1) {
     amtVal = 1;
     amount.value = "1";
   }
+
+  // console.log(fromCurr.value);
+  // console.log(toCurr.value);
+
   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+
   let response = await fetch(URL);
+  // console.log(response.status);
+
   let data = await response.json();
+  // console.log(data);
+
   let rate = data[toCurr.value.toLowerCase()];
+  // console.log(rate);
 
   let finalAmount = amtVal * rate;
   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 };
 
 const updateFlag = (element) => {
+  // console.log(element);
   let currCode = element.value;
+  // console.log(currCode);
   let countryCode = countryList[currCode];
   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
   let img = element.parentElement.querySelector("img");
